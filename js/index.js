3b22706fe2ab40fe0e9403887da84fbf3c74809f
kavangami03 <kavan.gami@softqubes.com>
2026-04-24 11:31:50 +0530

    Desing chnages Done

--- js/index.js
+++ js/index.js
@@ -17,4 +17,23 @@ jQuery(document).ready(function () {
       });
     }
   });
+
+  // Hero Slider Initialization
+  const heroSlider = new Swiper('.hero-slider', {
+    loop: true,
+    speed: 800,
+    // autoplay: {
+    //   delay: 5000,
+    //   disableOnInteraction: false,
+    // },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
 });
+
